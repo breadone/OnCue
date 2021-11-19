a80b91593a6802892d331c7e984fc1d91788408b
pradyun <pradyun.setti@gmail.com>
2021-11-19 14:21:58 +1300

    Initial Commit

---
+++ OnCue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnCue
+//
+//  Created by Pradyun Setti on 19/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnCue/OnCueApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnCueApp.swift
+//  OnCue
+//
+//  Created by Pradyun Setti on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct OnCueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
