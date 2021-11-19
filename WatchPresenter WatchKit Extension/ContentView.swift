733f354f1e89c76e28f5f116b3ef4f8c9bae33a5
pradyun <pradyun.setti@gmail.com>
2021-11-19 14:32:27 +1300

    got everything set up initially

---
+++ WatchPresenter WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchPresenter WatchKit Extension
+//
+//  Created by Pradyun Setti on 19/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Watch!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchPresenter WatchKit Extension/OnCueApp.swift
@@ -0,0 +1,19 @@
+//
+//  OnCueApp.swift
+//  WatchPresenter WatchKit Extension
+//
+//  Created by Pradyun Setti on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct OnCueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
