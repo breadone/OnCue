8d4e6827a0005cbee28aa8adccc52226484f3ed1
pradyun <pradyun.setti@gmail.com>
2021-11-30 19:21:13 +1300

    changed presviewphone so it looks better in dark and light modes, without having to use .preferredColorScheme() also changed the way the card positions appear so it works consistently (and is less complex)

--- OnCue/View/PresentationViewPhone.swift
+++ OnCue/View/PresentationViewPhone.swift
@@ -11,32 +11,34 @@ struct PresentationViewPhone: View {
     @Environment(\.colorScheme) var cs
     
     let project: Project
-    let darkModeColour = Color(white: 0.2, opacity: 0.9)
-    
-    @State private var titleText: String = ""
+    let lightModeColour = Color(white: 0.94, opacity: 1)
+    let darkModeColour = Color(white: 0.2, opacity: 0.7)
     
     var body: some View {
         GeometryReader { geo in
             ZStack {
                 TabView {
                     ForEach(project.cards, id: \.id) { card in
-                        Text(card.text)
-                            .font(.system(size: 25, weight: .medium, design: .default))
-                            .padding()
-                            .frame(width: geo.size.width * 0.95,
-                                   height: geo.size.height,// * 0.8,
-                                   alignment: .topLeading)
-                            .background(darkModeColour)
-                            .cornerRadius(17)
-                            .onAppear {
-                                self.titleText = "\(project.name) (\(card.position + 1)/\(project.cards.capacity))"
-                            }
+                        VStack(alignment: .leading) {
+                            Text("\(card.position + 1)")
+                                .foregroundColor(cs == .dark ? .white : .black)
+                                .font(.system(size: 15, weight: .light, design: .default))
+                            Text(card.text)
+                                .foregroundColor(cs == .dark ? .white : .black)
+                                .font(.system(size: 25, weight: .medium, design: .default))
+                        }
+                        .padding()
+                        .frame(width: geo.size.width * 0.95,
+                               height: geo.size.height,// * 0.8,
+                               alignment: .topLeading)
+                        .background(cs == .dark ? darkModeColour : lightModeColour)
+                        .cornerRadius(17)
                     }
                 }
                 .tabViewStyle(.page)
             }
         }
-        .navigationTitle(self.titleText).navigationBarTitleDisplayMode(.inline)
+        .navigationTitle(self.project.name).navigationBarTitleDisplayMode(.inline)
     }
 }
 
@@ -44,7 +46,7 @@ struct PresentationViewPhone_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             PresentationViewPhone(project: Project.testProject)
-                .preferredColorScheme(.dark)
+//                            .preferredColorScheme(.dark)
         }
     }
 }
